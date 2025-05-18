38180c1f91a1c550f97f055139eff4a318dd1ad1
BhuvanSharmAa <bhuvansharma971@gmail.com>
2025-05-18 09:18:21 +0530

    Utils folder updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ dotenv.config({
     path: './.env'
 });
 
-connectDB();
+connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+
+.catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+});
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,17 @@
+class ApiResponse {
+  constructor(status, message, data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
+
+  static success(data) {
+    return new ApiResponse(200, 'Success', data);
+  }
+
+  static error(message) {
+    return new ApiResponse(500, message, null);
+  }
+}
+
+export default ApiResponse;
